b85bcf8bc94a32240724498b6a36e494ed8fcae2
Sardorbek Imomaliev <3041675+imomaliev@users.noreply.github.com>
2025-09-15 21:08:59 +0100

    split main into ncopy and npaste files
    setup build for ncopy

--- build.zig
+++ build.zig
@@ -87,13 +87,13 @@ pub fn build(b: *std.Build) void {
     //
     // If neither case applies to you, feel free to delete the declaration you
     // don't need and to put everything under a single module.
-    const exe = b.addExecutable(.{
-        .name = "nclip",
+    const ncopy_exe = b.addExecutable(.{
+        .name = "ncopy",
         .root_module = b.createModule(.{
             // b.createModule defines a new module just like b.addModule but,
             // unlike b.addModule, it does not expose the module to consumers of
             // this package, which is why in this case we don't have to give it a name.
-            .root_source_file = b.path("src/main.zig"),
+            .root_source_file = b.path("src/ncopy.zig"),
             // Target and optimization levels must be explicitly wired in when
             // defining an executable or library (in the root module), and you
             // can also hardcode a specific target for an executable or library
@@ -114,23 +114,23 @@ pub fn build(b: *std.Build) void {
     });
 
     // add imports to exe
-    exe.root_module.addImport("clipboard", clipboard_dep.module("clipboard"));
-    exe.root_module.addImport("zlua", lua_dep.module("zlua"));
-    exe.root_module.addImport("sqlite", sqlite_dep.module("sqlite"));
-    exe.root_module.addImport("known_folders", known_folders_dep.module("known-folders"));
+    ncopy_exe.root_module.addImport("clipboard", clipboard_dep.module("clipboard"));
+    ncopy_exe.root_module.addImport("zlua", lua_dep.module("zlua"));
+    ncopy_exe.root_module.addImport("sqlite", sqlite_dep.module("sqlite"));
+    ncopy_exe.root_module.addImport("known_folders", known_folders_dep.module("known-folders"));
 
     // This declares intent for the executable to be installed into the
     // install prefix when running `zig build` (i.e. when executing the default
     // step). By default the install prefix is `zig-out/` but can be overridden
     // by passing `--prefix` or `-p`.
-    b.installArtifact(exe);
+    b.installArtifact(ncopy_exe);
 
     // This creates a top level step. Top level steps have a name and can be
     // invoked by name when running `zig build` (e.g. `zig build run`).
     // This will evaluate the `run` step rather than the default step.
     // For a top level step to actually do something, it must depend on other
     // steps (e.g. a Run step, as we will see in a moment).
-    const run_step = b.step("run", "Run the app");
+    const ncopy_step = b.step("ncopy", "Run ncopy app");
 
     // This creates a RunArtifact step in the build graph. A RunArtifact step
     // invokes an executable compiled by Zig. Steps will only be executed by the
@@ -138,17 +138,19 @@ pub fn build(b: *std.Build) void {
     // or if another step depends on it, so it's up to you to define when and
     // how this Run step will be executed. In our case we want to run it when
     // the user runs `zig build run`, so we create a dependency link.
-    const run_cmd = b.addRunArtifact(exe);
-    run_step.dependOn(&run_cmd.step);
+    const ncopy_cmd = b.addRunArtifact(ncopy_exe);
+    ncopy_step.dependOn(&ncopy_cmd.step);
 
     // By making the run step depend on the default step, it will be run from the
     // installation directory rather than directly from within the cache directory.
-    run_cmd.step.dependOn(b.getInstallStep());
+    ncopy_cmd.step.dependOn(b.getInstallStep());
 
     // This allows the user to pass arguments to the application in the build
     // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
-        run_cmd.addArgs(args);
+        if (std.mem.eql(u8, args[0], "ncopy")) {
+            ncopy_cmd.addArgs(args[1..]);
+        }
     }
 
     // Creates an executable that will run `test` blocks from the provided module.
@@ -164,19 +166,19 @@ pub fn build(b: *std.Build) void {
     // Creates an executable that will run `test` blocks from the executable's
     // root module. Note that test executables only test one module at a time,
     // hence why we have to create two separate ones.
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
+    const ncopy_exe_tests = b.addTest(.{
+        .root_module = ncopy_exe.root_module,
     });
 
     // A run step that will run the second test executable.
-    const run_exe_tests = b.addRunArtifact(exe_tests);
+    const run_ncopy_exe_tests = b.addRunArtifact(ncopy_exe_tests);
 
     // A top level step for running all tests. dependOn can be called multiple
     // times and since the two run steps do not depend on one another, this will
     // make the two of them run in parallel.
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
+    test_step.dependOn(&run_ncopy_exe_tests.step);
 
     // Just like flags, top level steps are also listed in the `--help` menu.
     //
---
+++ src/npaste.zig
@@ -0,0 +1,230 @@
+const std = @import("std");
+const builtin = @import("builtin");
+
+// third-party
+const clipboard_lib = @import("clipboard");
+const zlua = @import("zlua");
+const sqlite = @import("sqlite");
+const known_folders = @import("known_folders");
+
+// local
+const nclip_lib = @import("neoclipboard");
+
+const Lua = zlua.Lua;
+
+pub const known_folders_config: known_folders.KnownFolderConfig = .{
+    .xdg_on_mac = true,
+};
+
+// copied from zig's src/main.zig:69
+// This can be global since stdout is a singleton.
+// TODO: We needed writer buffer only for `sendFileAll`, but now we do not use it anymore
+// https://ziggit.dev/t/pr-24858-changed-sendfileall-and-now-it-always-requires-a-buffer-can-somebody-please-help-me-understand-why-this-ok/12046
+var stdout_buffer: [4096]u8 align(std.heap.page_size_min) = undefined;
+
+var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
+
+pub fn main() !u8 {
+    // // Prints to stderr, ignoring potential errors.
+    // try nclip_lib.bufferedPrint();
+
+    // copied from zig's src/main.zig
+    const gpa, const is_debug = gpa: {
+        switch (builtin.mode) {
+            .Debug => {
+                break :gpa .{ debug_allocator.allocator(), true };
+            },
+            else => {
+                break :gpa .{ std.heap.page_allocator, false };
+            },
+        }
+    };
+
+    defer if (is_debug) {
+        defer std.testing.expect(debug_allocator.deinit() == .ok) catch @panic("leak");
+    };
+
+    var arena_allocator = std.heap.ArenaAllocator.init(gpa);
+    defer arena_allocator.deinit();
+    const arena = arena_allocator.allocator();
+
+    const args = std.process.argsAlloc(gpa) catch {
+        std.debug.print("Failed to allocate args\n", .{});
+        return 2;
+    };
+    defer std.process.argsFree(gpa, args);
+
+    const cwd = std.fs.cwd();
+
+    // // Get the real path of the current working directory
+    // // https://github.com/ziglang/zig/issues/19353
+    // const cwd_path = try cwd.realpathAlloc(gpa, ".");
+    // defer gpa.free(cwd_path);
+    //
+    // // Join the cwd path with "db.sqlite"
+    // const db_path = try std.fs.path.join(gpa, &[_][]const u8{ cwd_path, "db.sqlite" });
+    // defer gpa.free(db_path);
+    //
+    // std.debug.print("Full path to db.sqlite: {s}\n", .{db_path});
+
+    const data_path_dir = try known_folders.open(gpa, known_folders.KnownFolder.data, .{});
+
+    _ = data_path_dir.?.access("nclip", .{}) catch {
+        try data_path_dir.?.makeDir("nclip");
+    };
+    const data_path = try data_path_dir.?.realpathAlloc(gpa, "nclip");
+    defer gpa.free(data_path);
+
+    const config_path_dir = try known_folders.open(gpa, known_folders.KnownFolder.local_configuration, .{});
+
+    _ = config_path_dir.?.access("nclip", .{}) catch {
+        try config_path_dir.?.makeDir("nclip");
+    };
+    const config_path = try config_path_dir.?.realpathAlloc(gpa, "nclip");
+    defer gpa.free(config_path);
+
+    const db_path = try std.fs.path.joinZ(gpa, &.{ data_path, "db.sqlite" });
+    // const db_path = try std.fs.path.join(gpa, &[_][]const u8{ data_path, "db.sqlite" });
+    defer gpa.free(db_path);
+
+    // std.debug.print("Full path to db.sqlite: {s}\n", .{db_path});
+
+    const db = try sqlite.Database.open(.{ .path = db_path });
+    defer db.close();
+
+    var storage: nclip_lib.Storage = .init(&db);
+    try storage.setup();
+
+    const exe = args[0];
+    var catted_anything = false;
+    var stdout_writer = std.fs.File.stdout().writerStreaming(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
+    // NOTE: I am not sure why in zig they are using buffered stdin, empty buffer works fine as well
+    var stdin_reader = std.fs.File.stdin().readerStreaming(&.{});
+
+    for (args[1..]) |arg| {
+        if (std.mem.eql(u8, arg, "-")) {
+            catted_anything = true;
+            const stdin = &stdin_reader.interface;
+            const input = try stdin.allocRemaining(gpa, .unlimited);
+            defer gpa.free(input);
+
+            try clipboard_lib.write(input);
+
+            try stdout.writeAll(input);
+            try stdout.flush();
+
+            var current_clipboard = nclip_lib.ClipboardModel{ .body = sqlite.text(input), .timestamp = std.time.timestamp() };
+            try storage.write(&current_clipboard);
+            return 0;
+        } else if (std.mem.eql(u8, arg, "-o")) {
+            // copy xclip's option name for now
+            try stdout.writeAll(clipboard_lib.read() catch @panic("can't read clipboard"));
+            try stdout.flush();
+            return 0;
+        } else if (std.mem.eql(u8, arg, "-h")) {
+            const clipboard = try storage.last(arena);
+            try stdout.writeAll(clipboard.body);
+            try stdout.flush();
+            return 0;
+        } else if (std.mem.eql(u8, arg, "-l")) {
+            const clipboards = try storage.list(arena);
+
+            for (clipboards.items) |clipboard| {
+                // print ending with NUL ascii to handle multi-line clipboards
+                try stdout.print("{s}\x00", .{ clipboard.body });
+            }
+            try stdout.flush();
+            return 0;
+        } else if (std.mem.eql(u8, arg, "-t")) {
+            // TODO: fix imports for lua files
+            // TODO: handle transform names
+            const stdin = &stdin_reader.interface;
+            const input = try stdin.allocRemaining(gpa, .unlimited);
+            defer gpa.free(input);
+
+            // Initialize the Lua vm
+            var lua = try Lua.init(gpa);
+            defer lua.deinit();
+
+            // https://luascripts.com/lua-embed
+            // https://piembsystech.com/integrating-lua-as-a-scripting-language-in-c-c-applications/
+            // https://piembsystech.com/working-with-modules-and-packages-in-lua-programming/
+            lua.openLibs();
+
+            const lua_path = try std.fs.path.joinZ(gpa, &.{ config_path, "lua", "init.lua" });
+            defer gpa.free(lua_path);
+            try lua.doFile(lua_path);
+            _ = try lua.getGlobal("trim_upper");
+            try lua.pushAny(input);
+            try lua.protectedCall(.{ .args = 1, .results = 1 });
+
+            const result = try lua.toString(1);
+
+            try clipboard_lib.write(result);
+
+            try stdout.writeAll(result);
+            try stdout.flush();
+
+            // TODO: save before transform, after transform, replace with transform
+            var current_clipboard = nclip_lib.ClipboardModel{ .body = sqlite.text(input), .timestamp = std.time.timestamp() };
+            try storage.write(&current_clipboard);
+
+            return 0;
+        } else if (std.mem.startsWith(u8, arg, "-")) {
+            return usage(exe);
+        } else {
+            const file = cwd.openFile(arg, .{}) catch |err| std.process.fatal("unable to open file: {t}\n", .{err});
+            defer file.close();
+
+            catted_anything = true;
+            var file_reader = file.reader(&.{});
+            const input = try file_reader.interface.allocRemaining(gpa, .unlimited);
+            defer gpa.free(input);
+
+            try clipboard_lib.write(input);
+
+            try stdout.writeAll(input);
+            try stdout.flush();
+
+            var current_clipboard = nclip_lib.ClipboardModel{ .body = sqlite.text(input), .timestamp = std.time.timestamp() };
+            try storage.write(&current_clipboard);
+        }
+    }
+    if (!catted_anything) {
+        const stdin = &stdin_reader.interface;
+        const input = try stdin.allocRemaining(gpa, .unlimited);
+        defer gpa.free(input);
+
+        try clipboard_lib.write(input);
+
+        try stdout.writeAll(input);
+        try stdout.flush();
+
+        var current_clipboard = nclip_lib.ClipboardModel{ .body = sqlite.text(input), .timestamp = std.time.timestamp() };
+        try storage.write(&current_clipboard);
+        return 0;
+    }
+    return 1;
+}
+
+fn usage(exe: []const u8) !u8 {
+    std.log.warn("Usage: {s} [FILE]...\n", .{exe});
+    return error.Invalid;
+}
+
+test {
+    std.testing.refAllDeclsRecursive(@This());
+}
+
+
+// test "fuzz example" {
+//     const Context = struct {
+//         fn testOne(context: @This(), input: []const u8) anyerror!void {
+//             _ = context;
+//             // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
+//             try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+//         }
+//     };
+//     try std.testing.fuzz(Context{}, Context.testOne, .{});
+// }
